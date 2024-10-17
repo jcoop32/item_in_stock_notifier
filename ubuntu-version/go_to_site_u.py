099072aaa067442b7ac971ed458075ab7b5b6e4c
jcoop32 <jcooper11@hawk.iit.edu>
2024-10-17 16:59:42 -0500

    fix inner texh check

--- ubuntu-version/go_to_site_u.py
+++ ubuntu-version/go_to_site_u.py
@@ -41,13 +41,13 @@ def go_to_site(url):
     time.sleep(2)
     add_to_cart_btn = driver.find_element(by=By.ID, value="product-addtocart-button")
     btn_inner_text = add_to_cart_btn.get_attribute("innerHTML")
-    print(f"Inner Text: {btn_inner_text}")
-    if btn_inner_text == "<span>Out Of Stock<span/>":
+    if btn_inner_text == "<span>Out Of Stock</span>":
         print("Says with span")
     elif btn_inner_text == "Out Of Stock":
         print("Says without span")
     else:
         print("Neither options")
+        print(btn_inner_text)
     print("STARTING REFRESH PHASE...")
     # for x in range(0, limit):
     #     countdown(int(60))
